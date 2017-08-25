f73b025bd44ce8f7354b30d9c87d2e772c7619d5
Issaret Prachitmutita <imac.monochrome@gmail.com>
2017-08-25 20:41:02 +0700

    Use v-model

--- src/components/HeroDetail.vue
+++ src/components/HeroDetail.vue
@@ -3,17 +3,21 @@
     <h2>{{hero.name}} details!</h2>
     <div>
       <label>id: </label>{{hero.id}}</div>
-
     <div>
       <label>name: </label>
-      <input placeholder="name">
+      <input v-model="hero.name" @change="onChange" placeholder="name">
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['hero']
+  props: ['hero'],
+  methods:{
+    onChange(){
+
+    }
+  }
 }
 </script>
 
